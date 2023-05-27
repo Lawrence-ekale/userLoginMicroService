790561fadf0192a50a757879c9b07616358fd3b8
Lawrence-ekale <lawrenceekale2@gmail.com>
2023-05-27 15:27:42 +0300

    Done with Authentication using Basic Auth

--- src/main/java/com/lawrenceekale/loginspringsecurity/controller/UserLoginController.java
+++ src/main/java/com/lawrenceekale/loginspringsecurity/controller/UserLoginController.java
@@ -1,10 +1,27 @@
 package com.lawrenceekale.loginspringsecurity.controller;
 
+import com.lawrenceekale.loginspringsecurity.entity.User;
+import com.lawrenceekale.loginspringsecurity.service.UserGeneralService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController()
 public class UserLoginController {
+    @Autowired
+    private UserGeneralService userGeneralService;
+
+    @PostMapping("/register")
+    public String registerUser(@RequestBody User user) {
+        return userGeneralService.registerUser(user);
+    }
+
+    @GetMapping("/")
+    public String generalPage() {
+        return "You are in general page";
+    }
 
     @GetMapping("/home")
     public String home() {
